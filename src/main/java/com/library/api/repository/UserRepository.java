0f5e30c40ed4d17fc8129c9db9e6791cee6fb5b6
Zhaaako01 <zhaako01@gmail.com>
2024-03-21 17:39:45 -0400

    - Added User and Role repositories and added some query methods in them

---
+++ src/main/java/com/library/api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.library.api.repository;
+
+import com.library.api.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+}
